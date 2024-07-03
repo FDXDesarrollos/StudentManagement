b8dce388cfdea3bb2a05f56e77b4ec816cf75065
fjuarez <fdxdesarrollos@gmail.com>
2024-07-03 14:37:58 -0600

    Finally proyect

--- src/main/java/com/fdxdesarrollos/studentManagement/util/PrintReport.java
+++ src/main/java/com/fdxdesarrollos/studentManagement/util/PrintReport.java
@@ -1,11 +1,9 @@
 package com.fdxdesarrollos.studentManagement.util;
 
-import java.io.InputStream;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Map;
-
-import javax.swing.JFrame;
 
 import com.fdxdesarrollos.studentManagement.App;
 
@@ -14,9 +12,11 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.swing.JRViewer;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
-public class PrintReport extends JFrame {
+public class PrintReport {
 	private String reporte = "";
 	private Map param = null;
 
@@ -26,27 +26,25 @@ public class PrintReport extends JFrame {
 	}
 	
 	public void showReport() {
+		String pathFile = "";
 		Connection conn = null;
 		
 		try {
 			conn = DBConnection.getInstance().getConnection();
-			InputStream stream = getClass().getResourceAsStream(App.class.getResource("report/" + reporte).getPath());
-			JasperReport jasperReport = JasperCompileManager.compileReport(stream);
-			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, param, conn);
-			JRViewer viewer = new JRViewer(jasperPrint);
-			viewer.setOpaque(true);
+			
+			pathFile = App.class.getResource("report/" + reporte).getPath();
+			//InputStream stream = new FileInputStream( new File(pathFile) );
+			JasperDesign design = JRXmlLoader.load(pathFile);
+			JasperReport report = JasperCompileManager.compileReport(design);
+			JasperPrint print = JasperFillManager.fillReport(report, param, conn);
+			JasperViewer viewer = new JasperViewer(print, false);
+			viewer.setTitle("Student Management System");
+			viewer.setSize(800, 600);
 			viewer.setVisible(true);
 			
-			this.add(viewer);
-			this.setSize(800, 600);
-			this.setVisible(true);
-			
-			//this.getContentPane().add(frame);
-			//this.setExtendedState(JFrame.);
-			//this.pack();
 		} catch (JRException ex) {
 			System.out.println("Error al generar reporte " + reporte + " " + ex.getMessage());
-			ex.getStackTrace();
+			ex.getStackTrace();	
 		} catch (SQLException ex) {
 			System.out.println("Error al consultar información " + ex.getMessage());
 			ex.getStackTrace();
@@ -59,3 +57,12 @@ public class PrintReport extends JFrame {
 		}		
 	}
 }
+
+/*************************************************************************/
+/* !!!  MUY IMPORTANTE  !!!
+ * 
+ * EN EL ARCHIVO ".jrxml" HAY QUE ELIMINAR EL ATRIBUTO 'language="groovy"'
+ * YA QUE DE LO CONTRARIO ESTO RPOVOCARA UN ERROR TODO EXTRAÑO Y
+ * EL REPORTE NO SE MOSTRARA !!! 
+ */
+/**************************************************************************/
